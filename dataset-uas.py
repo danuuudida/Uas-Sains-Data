a45510b70a1c47115ccf456f2a1a3bc411ec20ec
danuuudida <110648925+danuuudida@users.noreply.github.com>
2024-02-15 22:59:35 +0700

    Add files via upload

---
+++ dataset-uas.py
@@ -0,0 +1,79 @@
+#library
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import statsmodels.api as sm
+
+from factor_analyzer import FactorAnalyzer
+from sklearn import metrics
+from sklearn import preprocessing
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_predict
+from sklearn.model_selection import cross_val_score
+
+#load data
+@st.cache_data
+df_data_hour = pd.read_csv('hour.csv')
+df_data_day = pd.read_csv('day.csv')
+
+#mengakses data
+df_data_hour.info()
+df_data_hour.isnull().sum()
+
+#cleaning data
+df_data_hour = df_data_hour.dropna(how='any',axis=0)
+
+print("Null values removed successfully.")
+
+df_data_hour.isnull().sum()
+df_data_hour.duplicated().any()
+
+#explore data
+df_data_hour.rename(columns={'instant':'rec_id',
+                        'dteday':'datetime',
+                        'holiday':'is_holiday',
+                        'workingday':'is_workingday',
+                        'weathersit':'weather_condition',
+                        'hum':'humidity',
+                        'mnth':'month',
+                        'cnt':'total_count',
+                        'hr':'hour',
+                        'yr':'year'},inplace=True)
+
+
+df_data_hour['datetime'] = pd.to_datetime(df_data_hour.datetime)
+
+
+df_data_hour['season'] = df_data_hour.season.astype('category')
+df_data_hour['is_holiday'] = df_data_hour.is_holiday.astype('category')
+df_data_hour['weekday'] = df_data_hour.weekday.astype('category')
+df_data_hour['weather_condition'] = df_data_hour.weather_condition.astype('category')
+df_data_hour['is_workingday'] = df_data_hour.is_workingday.astype('category')
+df_data_hour['month'] = df_data_hour.month.astype('category')
+df_data_hour['year'] = df_data_hour.year.astype('category')
+df_data_hour['hour'] = df_data_hour.hour.astype('category')
+
+sns.set_style('whitegrid')
+sns.set_context('talk')
+params = {'legend.fontsize': 'x-large',
+          'figure.figsize': (30, 10),
+          'axes.labelsize': 'x-large',
+          'axes.titlesize':'x-large',
+          'xtick.labelsize':'x-large',
+          'ytick.labelsize':'x-large'}
+
+plt.rcParams.update(params)
+
+fig,ax = plt.subplots()
+sns.pointplot(data=df_data_hour[['hour',
+                           'total_count',
+                           'weekday']],
+              x='hour',
+              y='total_count',
+              hue='weekday',
+              ax=ax)
+ax.set(title="Distribusi hitungan per jam pada hari kerja")
