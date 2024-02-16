febd64d209075b744660ca5ea2be603e57bda5c0
danuuudida <110648925+danuuudida@users.noreply.github.com>
2024-02-17 03:27:25 +0700

    Update dataset-uas.py

--- dataset-uas.py
+++ dataset-uas.py
@@ -1,7 +1,7 @@
 #library
 import pandas as pd
 import numpy as np
-
+import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -14,6 +14,7 @@ from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_predict
 from sklearn.model_selection import cross_val_score
+from streamlit_option_menu import option_menu
 
 #load data
 @st.cache_data
